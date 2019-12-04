3bb56503e17ccc4c88f1a9a9be38f9a60da8e2b3
serhatcetinkaya <serhatcetinkaya@pm.me>
2019-12-04 19:17:13 +0300

    day four

---
+++ 4/part1/solution.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+declare -i MIN
+declare -i MAX
+declare -i NUM
+declare -i PREV
+declare -i DIGIT
+declare -i COUNT
+declare -i FLAG
+
+MIN=$(awk -F- '{ print $1 }' $1)
+MAX=$(awk -F- '{ print $2 }' $1)
+PREV=0
+COUNT=0
+FLAG=1
+
+for NUM in $(seq $MIN $MAX)
+do
+  for ((DIGIT=0; DIGIT < ${#NUM}; DIGIT++))
+  do
+    if [ "$PREV" -gt "${NUM:$DIGIT:1}" ]
+    then
+      FLAG=0
+      break
+    else
+      PREV=${NUM:$DIGIT:1}
+    fi
+  done
+  PREV=0
+  if [ "$FLAG" -eq 1 ] && grep -E '(.)\1+' <<< $NUM > /dev/null
+  then
+    COUNT=$COUNT+1
+  fi
+  FLAG=1
+done
+
+echo "Result: ${COUNT}"
